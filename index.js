6aa7e73c03199c6bafbc7a91b5fc05dac03ee49b
Cinan Rakosnik <cinan6@gmail.com>
2017-07-17 15:16:25 +0200

    Append hookfile.guppy to existing hookfile

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 
 require('shelljs/global');
 var fs = require('vinyl-fs');
+var nodeFs = require('fs');
 var map = require('map-stream');
 var async = require('async');
 var path = require('path');
@@ -27,36 +28,57 @@ var hooks = [
   'update'
 ];
 
-function install(hook, dest, cb) {
-  cb = cb || dest;
+function install(hook, destDir, cb) {
+  cb = cb || destDir;
 
   if (!cb) cb(new Error('Callback must be supplied.'));
   if (typeof cb !== 'function') cb(new Error('Callback must be a function.'));
   if (hooks.indexOf(hook) === -1) cb(new Error('Invalid hook name.'));
 
-  dest = ((typeof dest === 'function' ? null : dest) ||
+  destDir = ((typeof destDir === 'function' ? null : destDir) ||
     exec('git rev-parse --show-toplevel')
-      .output.slice(0, -1) + '/.git/hooks/');
+      .output.slice(0, -1) + (path.sep + path.join('.git', 'hooks') + path.sep));
 
-  var destHook = path.join(dest, hook);
+  var guppyHookFilename = `${hook}.guppy`;
+  var hookFilename = hook;
 
-  if (test('-f', destHook)) {
-    var bakDest = destHook + '.guppy';
+  var destGuppyHook = path.join(destDir, guppyHookFilename);
+  var destHook = path.join(destDir, hookFilename);
 
-    if (!test('-f', bakDest)) {
-      mv(destHook, bakDest);
-    }
-  }
-
-  return fs.src(path.join(__dirname, 'scripts/hookfile'))
+  return fs.src(path.join(__dirname, 'scripts/hookfile.guppy'))
     .pipe(map(function(file, next) {
-      file.path = file.path.replace('hookfile', hook);
+      file.path = file.path.replace('hookfile.guppy', guppyHookFilename);
       next(null, file);
     }))
-    .pipe(fs.dest(dest))
+    .pipe(fs.dest(destDir))
     .on('finish', function () {
-      chmod('u+x', destHook);
-      cb(null, destHook);
+      chmod('u+x', destGuppyHook);
+
+      var execGuppy = path.join('.git', 'hooks', guppyHookFilename);
+
+      if (nodeFs.existsSync(destHook)) {
+        var shebang = '#/usr/bin/env bash';
+
+        if (!grep(shebang, destHook).trim()) {
+          if (grep('#/usr/bin/env ', destHook).trim()) {
+              cb(null, destGuppyHook);
+              return;
+          }
+          echo(shebang + '\n').to(destHook);
+        }
+
+        if (!grep(execGuppy, destHook).trim()) {
+          echo(execGuppy).toEnd(destHook);
+        }
+
+        cb(null, destGuppyHook);
+      } else {
+        echo('#/usr/bin/env bash \n' +
+            execGuppy).to(destHook);
+        chmod('u+x', destHook);
+
+        cb(null, destGuppyHook);
+      }
     })
     .on('error', function (err) {
       cb(err);
--- test.js
+++ test.js
@@ -14,14 +14,16 @@ var gup = proxy('./index.js', {
 });
 var mv = function(){};
 
-test('install', function (t) {
-  t.plan(1);
+test('install clean', function (t) {
+  t.plan(2);
 
   gup.install('pre-commit', './', function () {
+    t.equal(sh.test('-f', './pre-commit.guppy'), true, 'should create pre-commit guppy hookfile');
     t.equal(sh.test('-f', './pre-commit'), true, 'should create pre-commit hookfile');
+
+    sh.rm('./pre-commit.guppy');
     sh.rm('./pre-commit');
   });
-
 });
 
 test('installAll', function (t) {
@@ -29,29 +31,27 @@ test('installAll', function (t) {
 
   gup.installAll('./', function (err, results) {
     if (err) t.fail('should not receive an error');
-    t.ok(sh.test('-f', './applypatch-msg'), 'should create applypatch-msg hookfile');
-    t.ok(sh.test('-f', './commit-msg'), 'should create commit-msg hookfile');
-    t.ok(sh.test('-f', './post-applypatch'), 'should create post-applypatch hookfile');
-    t.ok(sh.test('-f', './post-checkout'), 'should create post-checkout hookfile');
-    t.ok(sh.test('-f', './post-commit'), 'should create post-commit hookfile');
-    t.ok(sh.test('-f', './post-merge'), 'should create post-merge hookfile');
-    t.ok(sh.test('-f', './post-receive'), 'should create post-receive hookfile');
-    t.ok(sh.test('-f', './post-rewrite'), 'should create post-rewrite hookfile');
-    t.ok(sh.test('-f', './post-update'), 'should create post-update hookfile');
-    t.ok(sh.test('-f', './pre-applypatch'), 'should create pre-applypatch hookfile');
-    t.ok(sh.test('-f', './pre-auto-gc'), 'should create pre-auto-gc hookfile');
-    t.ok(sh.test('-f', './pre-commit'), 'should create pre-commit hookfile');
-    t.ok(sh.test('-f', './pre-push'), 'should create pre-push hookfile');
-    t.ok(sh.test('-f', './pre-rebase'), 'should create pre-rebase hookfile');
-    t.ok(sh.test('-f', './pre-receive'), 'should create pre-receive hookfile');
-    t.ok(sh.test('-f', './prepare-commit-msg'), 'should create prepare-commit-msg hookfile');
-    t.ok(sh.test('-f', './update'), 'should create update hookfile');
+    t.ok(sh.test('-f', './applypatch-msg.guppy'), 'should create applypatch-msg guppy hookfile');
+    t.ok(sh.test('-f', './commit-msg.guppy'), 'should create commit-msg guppy hookfile');
+    t.ok(sh.test('-f', './post-applypatch.guppy'), 'should create post-applypatch guppy hookfile');
+    t.ok(sh.test('-f', './post-checkout.guppy'), 'should create post-checkout guppy hookfile');
+    t.ok(sh.test('-f', './post-commit.guppy'), 'should create post-commit guppy hookfile');
+    t.ok(sh.test('-f', './post-merge.guppy'), 'should create post-merge guppy hookfile');
+    t.ok(sh.test('-f', './post-receive.guppy'), 'should create post-receive guppy hookfile');
+    t.ok(sh.test('-f', './post-rewrite.guppy'), 'should create post-rewrite guppy hookfile');
+    t.ok(sh.test('-f', './post-update.guppy'), 'should create post-update guppy hookfile');
+    t.ok(sh.test('-f', './pre-applypatch.guppy'), 'should create pre-applypatch guppy hookfile');
+    t.ok(sh.test('-f', './pre-auto-gc.guppy'), 'should create pre-auto-gc guppy hookfile');
+    t.ok(sh.test('-f', './pre-commit.guppy'), 'should create pre-commit guppy hookfile');
+    t.ok(sh.test('-f', './pre-push.guppy'), 'should create pre-push guppy hookfile');
+    t.ok(sh.test('-f', './pre-rebase.guppy'), 'should create pre-rebase guppy hookfile');
+    t.ok(sh.test('-f', './pre-receive.guppy'), 'should create pre-receive guppy hookfile');
+    t.ok(sh.test('-f', './prepare-commit-msg.guppy'), 'should create prepare-commit-msg guppy hookfile');
+    t.ok(sh.test('-f', './update.guppy'), 'should create update guppy hookfile');
 
     results.forEach(function (file) {
-      var bak = file + '.guppy';
-
       if (sh.test('-f', file)) sh.rm(file);
-      if (sh.test('-f', bak)) sh.rm(bak);
+      if (sh.test('-f', file.split('.guppy')[0])) sh.rm(file.split('.guppy')[0]);
     });
   });
 });
